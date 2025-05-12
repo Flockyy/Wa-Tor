1b063b9aa1f0416c9316d6ff6ddccbd3a879ca44
CVincent27 <cardon.vincent@hotmail.fr>
2025-05-12 14:43:46 +0200

    fix: fix  code requin

--- requin.py
+++ requin.py
@@ -12,7 +12,7 @@ class Requin(Poisson):
         points_total_vie (int): 6 par défaut. Nombre de chronons possibles sans manger. Points de vie de départ.
         points_par_repas (int): 3 par défaut. Nombre de chronons ajoutés aux points de vie lors d'un repas. 
     """
-    def __init__(self, cycle_reproduction: int = 12, points_total_vie: int = 6, points_par_repas: int = 4):
+    def __init__(self, cycle_reproduction: int = 12, points_total_vie: int = 12, points_par_repas: int = 6):
         super().__init__(cycle_reproduction)
         self.__points_energie = points_total_vie
         self.__point_total_vie = points_total_vie
@@ -35,49 +35,53 @@ class Requin(Poisson):
 
         if self.__points_energie == 0:
             ocean.effacer_valeur(coordonnees)
+        
+        else:
+            direction_choisie = Direction.Aucune
+            liste_directions = []
 
-        direction_choisie = Direction.Aucune
-        liste_directions = []
+            if self.points_energie < self.__point_total_vie:
+                liste_orientations = []
+                # Mode morfal : le requin détecte pour chaque directions quelle est la proie la plus proche...
+                for direction in Direction:
+                    if direction != Direction.Aucune:
+                        coordonnees_proie = self.rechercher_poisson(ocean, coordonnees, direction, "Proie")
+                        if coordonnees_proie != None:
+                            #... il calcule alors le chemin le plus court pour la choper...
+                            liste_orientations.append(ocean.calculer_orientation(coordonnees, coordonnees_proie))
+                if len(liste_orientations) > 0:
+                    # Il déduit les directions possibles triées par diner le plus proche...
+                    liste_orientations.sort(key=lambda orientation: orientation.distance)
+                    for orientation in liste_orientations:
+                        for direction in orientation.directions:
+                            if not (direction in liste_directions):
+                                liste_directions.append(direction)
 
-        if self.points_energie < self.__point_total_vie:
-            liste_orientations = []
-            # Mode morfal : le requin détecte pour chaque directions quelle est la proie la plus proche...
-            for direction in Direction:
-                if direction != Direction.Aucune:
-                    coordonnees_proie = self.rechercher_poisson(ocean, coordonnees, direction, "Proie")
-                    if coordonnees_proie != None:
-                        #... il calcule alors le chemin le plus court pour la choper...
-                        liste_orientations.append(ocean.calculer_orientation(coordonnees, coordonnees_proie))
-            if len(liste_orientations) > 0:
-                # Il déduit les directions possibles triées par diner le plus proche...
-                liste_orientations.sort(key=lambda orientation: orientation.distance)
-                for orientation in liste_orientations:
-                    for direction in orientation.directions:
-                        if not (direction in liste_directions):
-                            liste_directions.append(direction)
+            # on ajoute en dernier la direction du cycle précédent (nage en ligne droite par défaut)
+            liste_directions.append(self.direction)
 
-        # on ajoute en dernier la direction du cycle précédent (nage en ligne droite par défaut)
-        liste_directions.append(self.direction)
-
-        for direction in liste_directions:
-            if (ocean.infos_coordonnees(ocean.deplacer_coordonnees(coordonnees, direction)) != "Requin"):
-                if ocean.infos_coordonnees(ocean.deplacer_coordonnees(coordonnees, direction_choisie)) == "Proie":
-                    if self.points_energie < self.__point_total_vie:
+            for direction in liste_directions:
+                if (ocean.infos_coordonnees(ocean.deplacer_coordonnees(coordonnees, direction)) != "Requin"):
+                    if ocean.infos_coordonnees(ocean.deplacer_coordonnees(coordonnees, direction_choisie)) == "Proie":
+                        if self.points_energie < self.__point_total_vie:
+                            direction_choisie = direction
+                            break
+                    else:
                         direction_choisie = direction
                         break
-                else:
-                    direction_choisie = direction
-                    break
 
-        # et si les requins se bousculent, alors on prend la première direction possible
-        if direction_choisie == Direction.Aucune:
-            for direction in Direction:
-                if (ocean.infos_coordonnees(ocean.deplacer_coordonnees(coordonnees, direction)) != "Requin"):
-                    direction_choisie = direction
-                    break
-        if (ocean.infos_coordonnees(ocean.deplacer_coordonnees(coordonnees, direction_choisie)) == "Proie"):            
-            self.__points_energie += self.__points_par_repas
-        self.action_deplacement(coordonnees, direction_choisie, ocean)
+            # et si les requins se bousculent, alors on prend la première direction possible si il a faim
+            if direction_choisie == Direction.Aucune:
+                for direction in Direction:
+                    valeur_destination = ocean.infos_coordonnees(ocean.deplacer_coordonnees(coordonnees, direction))
+                    if not((valeur_destination == "Requin") or ((valeur_destination == "Proie") and (self.points_energie >= self.__point_total_vie))):
+                        direction_choisie = direction
+                        break
+                    
+            # On applique le choix final...
+            if (ocean.infos_coordonnees(ocean.deplacer_coordonnees(coordonnees, direction_choisie)) == "Proie"):            
+                self.__points_energie += self.__points_par_repas
+            self.action_deplacement(coordonnees, direction_choisie, ocean)
 
 
 
