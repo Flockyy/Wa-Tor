5bf15016f1f6a91884d423a65536e21ac8dde674
alexishs <a.halbotschoonaert@gmail.com>
2025-05-14 11:00:57 +0200

    main.py : gestion des arguments pour tous les paramètres + on applique tous les paramètres à la création de monde

--- main.py
+++ main.py
@@ -17,45 +17,98 @@ def main():
         """Parse les arguments de la ligne de commande."""
         parser = argparse.ArgumentParser(description="Wa-tor simulation")
         parser.add_argument(
+            "--menu",
+            type=str,
+            default="OUI",
+            help="Afficher le menu (OUI ou NON)"
+        )
+        parser.add_argument(
             "--auto",
             type=str,
             default="NON",
-            help="Automatiser la simulation (OUI ou NON)",
+            help="Automatiser la simulation (OUI ou NON)"
         )
         parser.add_argument(
             "--chronon",
             type=int,
             default=30,
-            help="Nombre d'étapes de simulation (cycle de vie)",
+            help="Nombre d'étapes de simulation si activation paramètre --auto=OUI (cycle de vie)"
         )
         parser.add_argument(
             "--hauteur",
             type=int,
             default=(10 if MODE_DEBUG else 30),
-            help="Nombre de lignes dans la grille",
+            help="Nombre de lignes dans la grille"
         )
         parser.add_argument(
             "--largeur",
             type=int,
             default=(10 if MODE_DEBUG else 30),
-            help="Nombre de colonnes dans la grille",
+            help="Nombre de colonnes dans la grille"
         )
         parser.add_argument(
-            "--proie",
+            "--nb-proie",
             type=int,
             default=(1 if MODE_DEBUG else 40),
-            help="Nombre de proies à placer dans la grille",
+            help="Nombre de proies à placer dans la grille"
         )
         parser.add_argument(
-            "--requin",
+            "--nb-requin",
             type=int,
             default=(0 if MODE_DEBUG else 10),
-            help="Nombre de requins à placer dans la grille",
+            help="Nombre de requins à placer dans la grille"
+        )
+        #parser.add_argument(
+        #    "--fichier",
+        #    type=str,
+        #    help="Fichier de sortie pour les résultats de la simulation",
+        #)
+        parser.add_argument(
+            "--cycle-reproduction-requin",
+            type=int,
+            default=(12 if MODE_DEBUG else 12),
+            help="Nombre de cycles entre chaque reproduction des requins"
         )
         parser.add_argument(
-            "--fichier",
+            "--cycle-reproduction-proie",
+            type=int,
+            default=(8 if MODE_DEBUG else 8),
+            help="Nombre de cycles entre chaque reproduction des proies"
+        )
+        parser.add_argument(
+            "--visibilite-requin",
             type=str,
-            help="Fichier de sortie pour les résultats de la simulation",
+            default=("OUI" if MODE_DEBUG else "OUI"),
+            help="Distance en cellules pour la vision des requins"
+        )
+        parser.add_argument(
+            "--visibilite-proie",
+            type=str,
+            default=("OUI" if MODE_DEBUG else "OUI"),
+            help="Distance en cellules pour la vision des proies"
+        )
+        parser.add_argument(
+            "--vue_arriere-requin",
+            type=str,
+            help="Capacité des requins à détecter les proies à distance derrière eux"
+        )
+        parser.add_argument(
+            "--vue_arriere-proie",
+            type=str,
+            default=("OUI" if MODE_DEBUG else "OUI"),
+            help="Capacité des proies à détecter les requins à distance derrière eux"
+        )
+        parser.add_argument(
+            "--points-de-vie-requin",
+            type=int,
+            default=(12 if MODE_DEBUG else 12),
+            help="Points de vie du requin (réduit de 1 à chaque cycle)"
+        )
+        parser.add_argument(
+            "--points-par-repas-requin",
+            type=int,
+            default=(6 if MODE_DEBUG else 6),
+            help="Points de recharge par proie mangée"
         )
 
         return parser.parse_args()
@@ -63,14 +116,12 @@ def main():
     args = parse_args()
 
     # Instanciation de la grille
-    monde = Monde(args.hauteur, args.largeur)
-
-    # Instanciation des poissons et requins
-    proie = [Proie(monde.ocean) for _ in range(args.proie)]
-    requins = [Requin(monde.ocean) for _ in range(args.requin)]
-
-    # Placement des poissons et requins dans la grille
-    monde.placer_poissons(proie, requins)
+    monde = Monde(args.hauteur, args.largeur,
+                  args.nb_requin, args.nb_proie,
+                  args.cycle_reproduction_requin, args.cycle_reproduction_proie,
+                  (args.visibilite_requin == "OUI"), (args.visibilite_proie == "OUI"),
+                  (args.vue_arriere_requin == "OUI"), (args.vue_arriere_proie == "OUI"),
+                  args.points_de_vie_requin, args.points_par_repas_requin)
 
     if args.auto.lower() == "oui":
         cnt = 0
